d43c4ac33dfeeb5a8106350791e6d7b04f057b18
borg2Borg <56142058+borg2borg@users.noreply.github.com>
2020-07-16 10:46:35 -0400

    Clean code, review and clean up add OOP later

---
+++ Example_Clean.py
@@ -0,0 +1,63 @@
+#  Table of Contents
+#  Week 1
+
+#  Week 1
+print("Hello World")  # print a str
+print(5 * 5)  # print a calc
+print(5 > 5)  # print boolean
+
+# Variable "x" equals a str of text and print to console
+x = "I'm programming in Python"
+print(x)
+
+# Print variable string to the console with an additional string
+name = "Eli"
+print("Hello, my " + name + ", it is nice to meet you.")
+
+# Negation of special characters, print variable strings to console
+color = "Greeen"
+thing = "Thing"
+print("The " + color + " " + thing + " \"Swamp " + thing + "\" " + "is one of the most terrifying beast of lore")
+
+# Additional Calcs, addition squared, power(s) and square root
+x = (((1 + 2) * 3)/ 4)**5
+print(x)
+
+y = 4**(1/2)
+print(y)
+
+# for
+friends = ['Taylor', 'Alex', 'Pat', 'Eli']
+for friend in friends:
+    print(friend)  # Print names within the list
+
+# For loop, while this condition is true, return value within the "Range" of 1 - 10
+for i in range(10):  # colon to start the statement
+    print("Hello world, again")  # Print str 10 times
+
+'''
+Cheat Sheet
+False	class	finally	is	return
+None	continue	for	lambda	try
+True	def	from	nonlocal	while
+and	del	global	not	with
+as	elif	if	or	yield
+assert	else	import	pass	
+break	except	in	raise	
+
+Operators 
+a + b = Adds a and b
+a - b = Subtracts b from a
+a * b = Multiplies a and b
+a / b = Divides a by b
+a ** b = Elevates a to the power of b. For non integer values of b, this becomes a root (i.e. a**(1/2) is the square root of a)
+a // b = The integer part of the integer division of a by b
+a % b = The remainder part of the integer division of a by b
+'''
+
+# Week 1 Quiz
+print("-----Week 1 Quiz -----")
+print("Programming in Python is, is like Godzilla wrecking it's favorite city building")
+
+ratio = ((1+5**(1/2))/2)
+print(ratio)
--- Examples.py
+++ Examples.py
@@ -972,12 +972,401 @@ for index, person in enumerate(winners):
     print("{} - {}".format(index +1, person))
 
 
-def skip_elementss(elements):
-    for index, element in enumerate(elements):
-    print("{} + {}".format(index +1, element))
+# def skip_elementss(elements):
+#    for index, element in enumerate(elements):
+#    print("{} + {}".format(index +1, element))
 
-    return skip_elements()
+#    return skip_elements()
 
 
-print(skip_elements(["a", "b", "c", "d", "e", "f", "g"]))  # Should be ['a', 'c', 'e', 'g']
-print(skip_elements(['Orange', 'Pineapple', 'Strawberry', 'Kiwi', 'Peach']))  # Should be ['Orange', 'Strawberry', 'Peach']
+#  print(skip_elements(["a", "b", "c", "d", "e", "f", "g"]))  # Should be ['a', 'c', 'e', 'g']
+#  print(skip_elements(['Orange', 'Pineapple', 'Strawberry', 'Kiwi', 'Peach']))
+
+
+def full_emails(people):
+    result = []
+    for email, name in people:
+        result.append("{} <{}>".format(name, email))
+    return result
+
+print(full_emails([("alex@example.com", "Alex Shay"), ("shaty@example.com", "Shaty May")]))
+
+multiples = []
+for x in range(1,11):
+    multiples.append(x*7)
+
+print(multiples)
+
+#  list comprehension
+multiples = [x * 7 for x in range(1,11)]
+print(multiples)
+
+languages = ["Python", "Perl", "Ruby", "Go", "Java", "C"]
+length = [len(language) for language in languages]
+print(length)
+
+z = [x for x in range(0,101) if x % 3 == 0]
+print(z)
+
+
+filenames = ["program.c", "stdio.hpp", "sample.hpp", "a.out", "math.hpp", "hpp.out"]
+# Generate newfilenames as a list containing the new filenames
+# using as many lines of code as your chosen method requires.
+newfilenames = [file.replace('.hpp', '.h') for file in filenames]
+
+print(newfilenames)
+# Should be ["program.c", "stdio.h", "sample.h", "a.out", "math.h", "hpp.out"]
+
+
+def pig_latin(text):
+    say = ""
+    # Separate the text into words
+    words = text.split()
+    for word in words:
+        # Create the pig latin word and add it to the list
+        say = say + word[1:] + word[0] + "ay "
+        # Turn the list back into a phrase
+    return say
+
+
+print(pig_latin("hello how are you"))  # Should be "ellohay owhay reaay ouyay"
+print(pig_latin("programming in python is fun"))  # Should be "rogrammingpay niay ythonpay siay unfay"
+
+
+def octal_to_string(octal):
+    result = ""
+    value_letters = [(4, "r"), (2, "w"), (1, "x")]
+    # Iterate over each of the digits in octal
+    for digit in [int(n) for n in str(octal)]:
+        # Check for each of the permissions values
+        for value, letter in value_letters:
+            if digit >= value:
+                result += letter
+                digit -= value
+            else:
+                result += '-'
+    return result
+
+print(octal_to_string(755))  # Should be rwxr-xr-x
+print(octal_to_string(644))  # Should be rw-r--r--
+print(octal_to_string(750))  # Should be rwxr-x---
+print(octal_to_string(600))  # Should be rw-------
+
+
+def group_list(group, users):
+  # members = users
+  return "{}: {}".format(group, ','.join(users))
+
+print(group_list("Marketing", ["Mike", "Karen", "Jake", "Tasha"])) # Should be "Marketing: Mike, Karen, Jake, Tasha"
+print(group_list("Engineering", ["Kim", "Jay", "Tom"])) # Should be "Engineering: Kim, Jay, Tom"
+print(group_list("Users", "")) # Should be "Users:"
+
+
+#def guest_list(guests):
+#    for x in guests:
+#        name, age, profession = x
+#  print("{} is {} years old and works as {}".format(name, age, profession))
+
+#  guest_list([('Ken', 30, "Chef"), ("Pat", 35, 'Lawyer'), ('Amanda', 25, "Engineer")])
+
+"""
+Output should match:
+Ken is 30 years old and works as Chef
+Pat is 35 years old and works as Lawyer
+Amanda is 25 years old and works as Engineer
+
+"""
+
+
+
+
+
+#
+#
+#
+#Lab 4
+def format_address(address_string):
+  # Declare variables
+  house_number = 0
+  street_name = ""
+  # Separate the address string into parts
+  separated_address = address_string.split()
+  # Traverse through the address parts
+  for address in separated_address:
+    # Determine if the address part is the
+    # house number or part of the street name
+    if address.isnumeric():
+      house_number = address
+  # Does anything else need to be done
+  # before returning the result?
+    else:
+      street_name += address + " "
+  # Return the formatted string
+  return "house number {} on street named {}".format(house_number, street_name)
+
+print(format_address("123 Main Street"))
+# Should print: "house number 123 on street named Main Street"
+
+print(format_address("1001 1st Ave"))
+# Should print: "house number 1001 on street named 1st Ave"
+
+print(format_address("55 North Center Drive"))
+# Should print "house number 55 on street named North Center Drive"
+
+
+
+def highlight_word(sentence, word):
+	return(sentence.replace(word, word.upper()))
+
+print(highlight_word("Have a nice day", "nice"))
+print(highlight_word("Shhh, don't be so loud!", "loud"))
+print(highlight_word("Automating with Python is fun", "fun"))
+
+
+def combine_lists(list1, list2):
+    # Generate a new list containing the elements of list2
+    # Followed by the elements of list1 in reverse order
+    list1.reverse()
+    list2.extend(list1)
+    return list2
+
+
+Jamies_list = ["Alice", "Cindy", "Bobby", "Jan", "Peter"]
+Drews_list = ["Mike", "Carol", "Greg", "Marcia"]
+
+print(combine_lists(Jamies_list, Drews_list))
+
+
+
+def squares(start, end):
+	return [ n*n for n in range(start, end+1) ]
+
+print(squares(2, 3)) # Should be [4, 9]
+print(squares(1, 5)) # Should be [1, 4, 9, 16, 25]
+print(squares(0, 10)) # Should be [0, 1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+def count_letters(text):
+    result = {}
+    for letter in text:
+        if letter not in result:
+            result[letter] = 0
+        result[letter] += 1
+    return result
+
+print(count_letters("aaaa"))
+print(count_letters("tenant"))
+print(count_letters("a long string"))
+
+
+def add_prices(basket):
+	# Initialize the variable that will be used for the calculation
+	total = 0
+	# Iterate through the dictionary items
+	for price in basket.values():
+		# Add each price to the total calculation
+		# Hint: how do you access the values of
+		# dictionary items?
+		total += price
+	# Limit the return value to 2 decimal places
+	return round(total, 2)
+
+groceries = {"bananas": 1.56, "apples": 2.50, "oranges": 0.99, "bread": 4.59,
+	"coffee": 6.99, "milk": 3.39, "eggs": 2.98, "cheese": 5.44}
+
+print(add_prices(groceries)) # Should print 28.44
+
+animal = "Hippopotamus"
+print(animal[3:6])
+print(animal[-5])
+print(animal[10:])
+
+
+colors = ["red", "white", "blue"]
+print(colors.insert(2, "yellow"))
+
+
+host_addresses = {"router": "192.168.1.1", "localhost": "127.0.0.1", "google": "8.8.8.8"}
+print(host_addresses.keys())
+
+
+def car_listing(car_prices):
+  result = ""
+  for name, price in car_prices.items():
+    result += "{} costs {} dollars".format(name, price) + "\n"
+  return result
+
+print(car_listing({"Kia Soul":19000, "Lamborghini Diablo":55000, "Ford Fiesta":13000, "Toyota Prius":24000}))
+
+
+
+def combine_guests(guests1, guests2):
+  new_guest_dictionary = {}
+  # Combine both dictionaries into one, with each key listed
+  new_guest_dictionary.update(guests2)
+  # only once, and the value from guests1 taking precedence
+  new_guest_dictionary.update(guests1)
+  return new_guest_dictionary
+Rorys_guests = { "Adam":2, "Brenda":3, "David":1, "Jose":3, "Charlotte":2, "Terry":1, "Robert":4}
+Taylors_guests = { "David":4, "Nancy":1, "Robert":2, "Adam":1, "Samantha":3, "Chris":5}
+
+print(combine_guests(Rorys_guests, Taylors_guests))
+
+
+
+def count_letters(text):
+  result = {}
+  text = text.replace(" ", "")
+  # Go through each letter in the text
+  for letter in text.lower():
+    # Check if the letter needs to be counted or not
+    if letter.isalpha():
+      if letter not in result:
+        result[letter] = 0
+      # Add or increment the value in the dictionary
+      result[letter] += 1
+  return result
+
+print(count_letters("AaBbCc"))
+# Should be {'a': 2, 'b': 2, 'c': 2}
+
+print(count_letters("Math is fun! 2+2=4"))
+# Should be {'m': 1, 'a': 1, 't': 1, 'h': 1, 'i': 1, 's': 1, 'f': 1, 'u': 1, 'n': 1}
+
+print(count_letters("This is a sentence."))
+# Should be {'t': 2, 'h': 1, 'i': 2, 's': 3, 'a': 1, 'e': 3, 'n': 2, 'c': 1}
+
+
+print(animal[3:6])
+print(animal[-5])
+print(animal[10:])
+
+
+colors = ["red", "white", "yellow", "blue"]
+colors.insert(2, "yellow")
+
+
+host_addresses = {"router": "192.168.1.1", "localhost": "127.0.0.1", "google": "8.8.8.8"}
+host_addresses.keys()
+
+
+# Dictionaries
+
+# pairs and key values, str, int, tuples etc
+#  Mutable
+
+#Key - Word (wurd) (n.)
+#Definition or Value 1. a unit of language....
+
+x = {}
+type(x)
+print(type(x))
+
+file_counts = {"jpg":10, "txt":14, "csv":2, "py":23}
+print(file_counts)
+
+print(file_counts["txt"])
+print("jpg" in file_counts)  # T or F is JPG in the dictionary?
+print("java" in file_counts)  # Same as above but is java?
+
+file_counts["cfg"] = 8
+print(file_counts)
+
+file_counts["csv"] = 17
+print(file_counts)  # value replaces 17 for 2, are unique
+
+del file_counts["cfg"]
+print(file_counts)
+
+# Example
+toc = {"Introduction":1, "Chapter 1":4, "Chapter 2":11, "Chapter 3":25, "Chapter 4":30}
+toc["Epilogue"] = 39 # Epilogue starts on page 39
+toc["Chapter 3"] = 24 # Chapter 3 now starts on page 24
+print(toc) # What are the current contents of the dictionary?
+print("Chapter 5" in toc) # Is there a Chapter 5?
+
+#input Key "" value:X
+file_counts = {"jpg":10, "txt":14, "csv":2, "py":23}
+for extension in file_counts:
+    print(extension)
+
+for ext, amount in file_counts.items():
+    print("There are {} files with the .{} extension".format(amount, ext))
+
+#file_counts.keys()
+#dict_keys(['jpg', 'txt', 'csv', 'py'])
+#file_counts.values()
+#dict_values([10, 14, 2, 23])
+#for value in file_counts.values():
+#    print(value)
+
+#set - store values only present once and immutable
+
+#cool_beasts = {"octopuses": "tentacles", "dolphins": "fins", "rhinos": "horns"}
+#for item in cool_beasts.items():
+#print("{} have {}".format(item))
+
+
+def count_letters(text):
+    result = {}
+    for letter in text:
+        if letter not in result:
+            result[letter] = 0
+        result[letter] += 1
+    return result
+
+print(count_letters("aaaa"))
+print(count_letters("tenant"))
+print(count_letters("a long string"))
+
+
+# Lists - ip addresses or hostnames and ip addresses,any data type
+# dictionaries - usernames, groups - specific element any data type for value but keys have to be specific
+# numbers, booleans, strings or tuples dict keys
+#keys - keys, or even dictionaries
+
+
+def email_list(domains):
+	emails = []
+	for domain_name, users in domains.items():
+	  for user in users:
+	    emails.append(user + "@" + domain_name)
+	return(emails)
+
+print(email_list({"gmail.com": ["clark.kent", "diana.prince", "peter.parker"], "yahoo.com": ["barbara.gordon", "jean.grey"], "hotmail.com": ["bruce.wayne"]}))
+
+
+def groups_per_user(group_dictionary):
+	user_groups = {}
+	# Go through group_dictionary
+	for group, users in group_dictionary.items():
+		# Now go through the users in the group
+		for user in users:
+			if user in user_groups:
+				user_groups[user].append(group)
+			else:
+				user_groups[user] = [group]
+	return(user_groups)
+
+print(groups_per_user({"local": ["admin", "userA"],
+		"public":  ["admin", "userB"],
+		"administrator": ["admin"] }))
+
+
+
+
+def add_prices(basket):
+	# Initialize the variable that will be used for the calculation
+	total = 0
+	# Iterate through the dictionary items
+	for price in basket.values():
+		# Add each price to the total calculation
+		# Hint: how do you access the values of
+		# dictionary items?
+		total += price
+	# Limit the return value to 2 decimal places
+	return round(total, 2)
+
+groceries = {"bananas": 1.56, "apples": 2.50, "oranges": 0.99, "bread": 4.59,
+	"coffee": 6.99, "milk": 3.39, "eggs": 2.98, "cheese": 5.44}
+
+print(add_prices(groceries)) # Should print 28.44
